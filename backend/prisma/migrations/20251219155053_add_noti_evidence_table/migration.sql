7b0f4dd7ef88eeaccb19d74f321ca3572619ccec
Kzt <tun81541@gmail.com>
2026-09-12 20:48:20 +0630

    modify_prisma_provider

--- backend/prisma/migrations/20251219155053_add_noti_evidence_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "NotiEvidence" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "authorId" INTEGER NOT NULL,
-    "notiToken" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "NotiEvidence_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- backend/prisma/migrations/20260623151923_modify_modtrack_table_v3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[updateUserId,taskId]` on the table `ModTracker` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ModTracker_updateUserId_taskId_key" ON "ModTracker"("updateUserId", "taskId");
